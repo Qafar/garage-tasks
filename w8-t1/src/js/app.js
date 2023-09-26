f4ccaa9d2252acdaf426bd1d57043c21b087e9bf
Qafar <qafar313@gmail.com>
2023-09-26 14:37:53 +0400

    Scripted input task

--- w8-t1/src/js/app.js
+++ w8-t1/src/js/app.js
@@ -11,3 +11,23 @@ function hideLoading() {
 setTimeout(hideLoading, 5000);
 
 window.addEventListener("load", showLoading);
+
+//input task
+
+function handleKeyPress(e) {
+    if (e.key === " " || e.key === "Backspace") {
+        e.preventDefault();
+
+        const input = document.getElementById("myInput");
+        const cursorPosition = input.selectionStart;
+        const inputValue = input.value;
+
+        if (e.key === " ") {
+            input.value = inputValue.slice(0, cursorPosition) + inputValue.slice(cursorPosition + 1);
+            input.setSelectionRange(cursorPosition, cursorPosition);
+        } else if (e.key === "Backspace") {
+            input.value = inputValue.slice(0, cursorPosition) + " " + inputValue.slice(cursorPosition);
+            input.setSelectionRange(cursorPosition + 1, cursorPosition + 1);
+        }
+    }
+}
